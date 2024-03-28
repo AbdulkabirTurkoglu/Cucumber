f1081d97a648e0e1e8a76ea613eeadf509d77868
Abdulkabir <turkogluabdulkabir@gmail.com>
2024-03-29 02:31:12 +0300

    locatorlar bulundu

---
+++ src/test/java/StepDefinitions/_03_CitizenShip.java
@@ -0,0 +1,14 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+
+public class _03_CitizenShip {
+    @And("Navigate to CitizenShip")
+    public void navigateToCitizenShip() {
+    }
+
+    @When("Create a CitizenShip")
+    public void createACitizenShip() {
+    }
+}
---
+++ src/test/java/pages/LeftNav.java
@@ -0,0 +1,35 @@
+package pages;
+
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LeftNav extends ParentPage{
+
+    public LeftNav() {
+        PageFactory.initElements(GWD.getDriver(),this);
+    }
+
+    @FindBy(xpath = "(//span[text()='Setup'])[1]")
+    public WebElement setup;
+
+    @FindBy(xpath = "//span[text()='Parameters']")
+    public WebElement parameters;
+
+    @FindBy(xpath = "(//span[text()='Countries'])[1]")
+    public WebElement countries;
+
+
+
+    public WebElement getWebElement(String strElement){
+        switch (strElement){
+            case "setup": return this.setup;
+            case "parameters":return this.parameters;
+            case "countries":return this.countries;
+        }
+        return null;
+    }
+
+
+}
