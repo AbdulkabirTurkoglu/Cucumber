6506245c10601f23b59d2f0b8fd46c10cc06b73b
Abdulkabir <turkogluabdulkabir@gmail.com>
2024-03-28 17:25:46 +0300

    login

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -2,18 +2,29 @@ package StepDefinitions;
 
 import Utilities.GWD;
 import io.cucumber.java.en.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.DialogContent;
+
+import java.time.Duration;
 
 public class _01_LoginSteps {
+    DialogContent dc = new DialogContent();
     @Given("Navigate to Campus")
     public void navigate_to_campus() {
         System.out.println("Site açıldı");
-        GWD.getDriver().get("");
-
-
+        GWD.getDriver().get("https://test.mersys.io/");
     }
     @When("Enter username and password and click login button")
     public void enter_username_and_password_and_click_login_button() {
+        WebDriverWait wait=new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.visibilityOf(dc.username));
+
+
         System.out.println("username ve şifre gönderildi");
+        dc.username.sendKeys("turkeyts");
+        dc.password.sendKeys("TechnoStudy123");
+        dc.loginButton.click();
 
 
 
